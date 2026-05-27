8390dd7167654546e28f7c4769c633cb807bb0b7
LMaerkens <l.maerkens@icloud.com>
2026-05-27 09:00:34 +0200

    registreer pagina

--- app/Http/Controllers/gebruikercontroller.php
+++
@@ -1,9 +0,0 @@
-<?php
-class gebruikercontroller extends Controller
-{
-    public function index()
-    {
-        return view('registreer.index');
-    }
-}
-
--- resources/views/registreer.blade.php
+++ resources/views/registreer.blade.php
@@ -1,13 +1,30 @@
-<?php
-
-namespace App\Http\Controllers;
-
-use Illuminate\Http\Request;
-
-class GebruikerController extends Controller
-{
-    public function index()
-    {
-        return view('registreer');
-    }
-}
+<!doctype html>
+<html lang="nl">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Registreer</title>
+    <link rel="stylesheet" href="/css/app.css">
+ </head>
+<body>
+    <main style="padding:2rem;">
+        <h1>Registreer</h1>
+        <form method="POST" action="/register">
+            @csrf
+            <div>
+                <label for="name">Naam</label>
+                <input id="name" name="name" required>
+            </div>
+            <div>
+                <label for="email">E-mail</label>
+                <input id="email" name="email" type="email" required>
+            </div>
+            <div>
+                <label for="password">Wachtwoord</label>
+                <input id="password" name="password" type="password" required>
+            </div>
+            <button type="submit">Registreer</button>
+        </form>
+    </main>
+</body>
+</html>
