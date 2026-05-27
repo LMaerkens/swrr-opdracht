248eb503e2a84b056036a4e761ceaecbed62f201
niekjepiekje <502760@student.firda.nl>
2026-05-27 09:03:46 +0200

    oke

--- app/Http/Controllers/gebruikercontroller.php
+++ app/Http/Controllers/gebruikercontroller.php
@@ -1,9 +1,37 @@
 <?php
-class gebruikercontroller extends Controller
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class GebruikerController extends Controller
 {
     public function index()
     {
-        return view('registreer.index');
+        return view('registreer');
+    }
+
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'voornaam' => 'required|string|max:255',
+            'achternaam' => 'required|string|max:255',
+            'lidmaatschap' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users,email',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+
+        User::create([
+            'voornaam' => $data['voornaam'],
+            'achternaam' => $data['achternaam'],
+            'lidmaatschap' => $data['lidmaatschap'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password']),
+        ]);
+
+        return redirect()->route('registreer.form')->with('success', 'Account is aangemaakt!');
     }
 }
 
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GebruikerController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/registreer', [GebruikerController::class, 'index'])->name('registreer.form');
+Route::post('/registreer', [GebruikerController::class, 'store'])->name('registreer.submit');
