96f6351b59ca060ec1c90eaedc2d257b3ec88d83
niekjepiekje <502760@student.firda.nl>
2026-05-29 10:24:36 +0200

    home

---
+++ resources/views/huisje.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Huisjes</title>
+</head>
+<body>
+
+    <h1>Onze Huisjes</h1>
+
+    <div>
+        <h2>Texel Huisje</h2>
+        <p>Mooie locatie dichtbij zee.</p>
+        <p>Prijs: €250</p>
+    </div>
+
+</body>
+</html>
