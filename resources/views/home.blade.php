aa69e1f7360abd9515feca883ce000feb80777b5
niekjepiekje <502760@student.firda.nl>
2026-06-02 13:28:39 +0200

    commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <html lang="nl">
-
+ 
 <head>
-
+<
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
@@ -12,6 +12,13 @@
     <link
         href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
         rel="stylesheet">
+
+    <style>
+        .btn {
+            font-size: 1.05rem;
+            padding: 0.75rem 1.25rem;
+        }
+    </style>
 
 </head>
 
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 
 <?php
+ use App\Http\Controllers\GebruikerController;
 Route::get('/', function () {
     return view('home');
 });
