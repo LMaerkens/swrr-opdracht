eb598e8b8c60927764177d9a9e65f5fe3cd3f919
niekjepiekje <502760@student.firda.nl>
2026-06-05 09:39:19 +0200

    hehe

--- routes/web.php
+++ routes/web.php
@@ -1,37 +1,37 @@
-<?php
+    <?php
 
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\LoginController;
-use App\Http\Controllers\HuisjeController;
+    use Illuminate\Support\Facades\Route;
+    use App\Http\Controllers\UserController;
+    use App\Http\Controllers\LoginController;
+    use App\Http\Controllers\HuisjeController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-*/
+    /*
+    |--------------------------------------------------------------------------
+    | Web Routes
+    |--------------------------------------------------------------------------
+    */
 
-// Home page
-Route::get('/', function () {
-    return view('home');
-})->name('home');
+    // Home page
+    Route::get('/', function () {
+        return view('home');
+    })->name('home');
 
-// Authentication Routes
-Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
-Route::post('/login', [LoginController::class, 'login']);
-Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+    // Authentication Routes
+    Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+    Route::post('/login', [LoginController::class, 'login']);
+    Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 
-// Registration Routes
-Route::get('/registreer', [UserController::class, 'index'])->name('registreer.form');
-Route::post('/registreer', [UserController::class, 'store'])->name('registreer.submit');
-Route::get('/register', [UserController::class, 'index'])->name('register');
-Route::post('/register', [UserController::class, 'store']);
+    // Registration Routes
+    Route::get('/registreer', [UserController::class, 'index'])->name('registreer.form');
+    Route::post('/registreer', [UserController::class, 'store'])->name('registreer.submit');
+    Route::get('/register', [UserController::class, 'index'])->name('register');
+    Route::post('/register', [UserController::class, 'store']);
 
-// Huisjes Routes
-Route::resource('huisjes', HuisjeController::class)->except(['show']);
+    // Huisjes Routes
+    Route::resource('huisjes', HuisjeController::class)->except(['show']);
 
-// Boeking Page
-Route::view('/boeking', 'boeking.index')->name('boeking');
+    // Boeking Page
+    Route::view('/boeking', 'boeking.index')->name('boeking');
 
-// Voorwaarden Page
-Route::view('/voorwaarden', 'voorwaarden')->name('voorwaarden');
+    // Voorwaarden Page
+    Route::view('/voorwaarden', 'voorwaarden')->name('voorwaarden');
